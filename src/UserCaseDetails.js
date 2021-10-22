fe35e789693ce918eec4d1a61d1a9b4da0ba49fe
Andrew Nguyen <anguyen0615@gmail.com>
2021-10-22 13:31:09 -0500

    Updates description and components
    Updated the website description and moved the email section into a reusable component

--- src/UserCaseDetails.js
+++ src/UserCaseDetails.js
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Spinner, Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Spinner, Container, Row, Col } from 'react-bootstrap';
 import { useParams } from "react-router";
-import { url } from "./constants/global"
+import { url } from "./constants/global";
+import EmailInfo from './EmailInfo';
 import useFetch from "./useFetch";
 
 
@@ -62,26 +63,7 @@ const UserCaseDetails = () => {
                             </div>
                         </div>
                     </div>
-
-                    <Container className="">
-                        <Row className="justify-content-center align-items-center">
-                            <Col xs="12" sm="12" md="6" lg="4" xl="4">
-                                <Form className="">
-                                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                                        <Form.Label>Have the information sent to your email!</Form.Label>
-                                        <Form.Control type="email" placeholder="Enter email" />
-                                        <Form.Text className="text-muted">
-                                        We'll never share your email with anyone else.
-                                        </Form.Text>
-                                    </Form.Group>
-
-                                    <Button variant="primary" type="submit">
-                                        Submit
-                                    </Button>
-                                </Form>
-                            </Col>
-                        </Row>
-                    </Container>
+                    <EmailInfo />
                 </article>
             )}
         </div>
